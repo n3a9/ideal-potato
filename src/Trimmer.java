29945cdc02daf71c77ccfddc11cf607697f6f9bc
Neeraj Aggarwal <aggarwalneeraj141@users.noreply.github.com>
2017-01-05 08:56:38 -0800

    update trimmer with comments and headers

--- src/Trimmer.java
+++ src/Trimmer.java
@@ -1,9 +1,16 @@
-/**
- * Created by neerajaggarwal on 1/4/17.
- */
+import java.util.Arrays;
+
 public class Trimmer {
 
-    public void dataTrimmer() {
-        return;
+    //return an array consisting of the data set without outliers
+    public int[] dataTrimmer(int[] data) {
+        return data; //to quiet error
+    }
+
+
+    //return an array consisting of the outliers in the data set
+    public int[] findOutliers(int[] data) {
+        Arrays.sort(data);
+        return data; //to quiet error
     }
 }
